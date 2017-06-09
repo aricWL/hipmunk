f2b3ace0bb7a8dd6535283470ede9b6444ac3a1c
aricWL <aricliesenfelt@gmail.com>
2017-06-08 17:25:29 -0700

    Refactored views.py, removed clutter files

--- project/hotels/views.py
+++ project/hotels/views.py
@@ -1,4 +1,4 @@
-import requests
+import grequests
 import json
 from flask import Blueprint, jsonify
 from flask_restful import Api, Resource
@@ -13,23 +13,27 @@ hotels_api = Api(hotels_blueprint)
 @hotels_api.resource('/search')
 class SearchAPI(Resource):
     def get(self):
-        merged = []
-        results = [json.loads(requests.get('http://localhost:9000/scrapers/expedia').content)['results'],
-                    json.loads(requests.get('http://localhost:9000/scrapers/orbitz').content)['results'],
-                    json.loads(requests.get('http://localhost:9000/scrapers/priceline').content)['results'],
-                    json.loads(requests.get('http://localhost:9000/scrapers/travelocity').content)['results'],
-                    json.loads(requests.get('http://localhost:9000/scrapers/hilton').content)['results']]
 
-        results = [result for result in results if len(result) > 0]
-        from IPython import embed; embed()
+        merged = []
+        urls = ['expedia', 'orbitz', 'priceline', 'travelocity', 'hilton']
+        response_shells = [grequests.get('http://localhost:9000/scrapers/'+ u) for u in urls]
+        responses = grequests.map(response_shells)
+        results = [json.loads(response.content)['results'] for response in responses]
+
         while len(results) > 0:
-            lowest_ecstacies = [result[-1]['ecstasy'] for result in results]
-            low_index = lowest_ecstacies.index(min(lowest_ecstacies))
+            # Getting current min from each result
+            current_mins = [result[-1]['ecstasy'] for result in results]
+            # Getting the min of the current mins
+            low_index = current_mins.index(min(current_mins))
+            # Adding min to merged list
             merged.append(results[low_index][-1])
+            # Removing the merged min
             results[low_index].pop()
+            # If that results list is now empty, remove it
             if len(results[low_index]) == 0:
                 results.pop(low_index)
-
+                
+        # The list will be in reverse because we used push()
         merged.reverse()
 
         return jsonify({'results': merged})
